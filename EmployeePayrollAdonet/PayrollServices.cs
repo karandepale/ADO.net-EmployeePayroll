e002192c61e481c28e500e74563a068fd7a3bfb2
“karandepale” <karandepale111@gmail.com>
2023-06-24 15:20:17 +0530

    [Karan]:Table created

--- EmployeePayrollAdonet/PayrollServices.cs
+++ EmployeePayrollAdonet/PayrollServices.cs
@@ -26,5 +26,25 @@ namespace EmployeePayrollAdonet
             }
             
         }
+
+        public void CreateTable()
+        {
+            try {
+                SqlConnection con = new SqlConnection(@"data source=DESKTOP-HDRGJGO\SQLEXPRESS; initial catalog=Payroll_Service; integrated security=true;");
+                con.Open();
+
+                string query = "create table employee_payroll(Columns_Id int identity(1,1) Primary key,Name varchar(20),Salary varchar(10),StartDate date,Note_Id int)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Table Created Sussessfully...");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+
     }
 }
--- EmployeePayrollAdonet/Program.cs
+++ EmployeePayrollAdonet/Program.cs
@@ -9,7 +9,8 @@ namespace EmployeePayrollAdonet
             Console.WriteLine("Employee payroll Connectivity Database with .Net applications using Ado.Net!");
 
             PayrollServices obj = new PayrollServices();
-            obj.CreateDataBase();
+            // obj.CreateDataBase();
+            obj.CreateTable();
         }
     }
 }
